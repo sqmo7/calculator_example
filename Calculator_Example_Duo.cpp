ef4a88009dffd54bcddb657e6d2996d7924b9ce4
samet aktu <sametaktu1903@gmail.com>
2025-10-22 10:58:23 +0300

    version 2.0

--- Calculator_Example_Duo.cpp
+++ Calculator_Example_Duo.cpp
@@ -11,7 +11,7 @@ int main()
     cin >> _sayi1;
     cout << endl << endl;
 
-    cout << "islem türü (1:+, 2:-, 3:*, 4:/)";
+    cout << "islem turu (1:+, 2:-, 3:*, 4:/)";
     cin >> _secilenIslem;
     cout << endl << endl; 
 
@@ -19,5 +19,33 @@ int main()
     cin >> _sayi2;
     cout << endl << endl;
 
-    return(0)
+        if(_secilenIslem == 1)
+    {
+        _sonuc = _sayi1 + _sayi2;
+        cout << "sonuc:" << _sonuc;
+    }
+
+    else if(_secilenIslem == 2)
+    {
+        _sonuc = _sayi1 - _sayi2;
+        cout << "sonuc:" << _sonuc;
+    }
+
+    else if(_secilenIslem == 3)
+    {
+        _sonuc = _sayi1 * _sayi2;
+        cout << "sonuc:" << _sonuc;
+    }
+
+    else if(_secilenIslem == 4 && _sayi2 != 0)
+    {
+        _sonuc = _sayi1 / _sayi2;
+        cout << "sonuc:" << _sonuc;
+    }
+
+else
+{
+
+}
+    return 0;
 }
