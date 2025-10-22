7ecdeccf3b04f718014fe5326412c25e57104169
derdevz <220624386+derdevz@users.noreply.github.com>
2025-10-22 09:27:02 +0300

    v0.1 Variable Added and First Commit

---
+++ Calculator_Example_Duo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int _sayi1, _sayi2, _sonuc, _secilenIslem;
+    string _hataliIslem;
+
+    cout << "1. sayi:";
+    cin >> _sayi1;
+    cout << endl << endl;
+
+    cout << "islem türü (1:+, 2:-, 3:*, 4:/)";
+    cin >> _secilenIslem;
+    cout << endl << endl; 
+
+    cout << "2. sayi:";
+    cin >> _sayi2;
+    cout << endl << endl;
